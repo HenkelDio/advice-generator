352c408bb11d45e9b3447332ffd5d46afa854a82
HenkelDIo <willianhenkel@gmail.com>
2022-06-15 15:16:20 -0300

    newversion2

---
+++ src/components/Card.jsx
@@ -0,0 +1,52 @@
+const Card = () => {
+
+
+        function Number(){
+        fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(advices =>{
+                
+        var id = advices.slip.id
+        var advice = advices.slip.advice
+
+        console.log(id)
+        console.log(advice)
+
+        var numberAdvice = document.querySelector('.header-card h3 span')
+        var contentAdvice = document.querySelector('.content-card p')
+
+        numberAdvice.innerHTML = id
+        contentAdvice.innerHTML = advice
+
+        
+            
+        })
+    
+    }
+
+    return (
+        <div className="card">
+
+            <div className="card-wrapper">
+                
+                <div className="header-card">
+                    <h3>ADVICE #<span></span></h3>
+                </div>
+                <div className="content-card">
+                    <p>
+
+                    </p>
+                </div>
+            </div>
+            <div className="divider-desktop">
+                <img src="./content/images/pattern-divider-desktop.svg"></img>
+            </div>
+            <div className="dice" onClick={Number}>
+                <img src='./content/images/icon-dice.svg'></img>
+            </div>
+
+        </div>
+    )
+}
+
+export default Card
